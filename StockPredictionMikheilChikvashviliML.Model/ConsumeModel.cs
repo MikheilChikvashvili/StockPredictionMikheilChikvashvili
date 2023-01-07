d0dffedd25485949e552d36cf3f4296146d6449a
MikheilChikvashvili <36455404+MikheilChikvashvili@users.noreply.github.com>
2023-01-07 19:53:41 +0400

    MLdata path update
    Updated path for the MLDATA.zip file.

--- StockPredictionMikheilChikvashviliML.Model/ConsumeModel.cs
+++ StockPredictionMikheilChikvashviliML.Model/ConsumeModel.cs
@@ -27,7 +27,7 @@ namespace StockPredictionMikheilChikvashviliML.Model
             MLContext mlContext = new MLContext();
 
             // Load model & create prediction engine
-            string modelPath = @"C:\Users\user\source\repos\StockPredictionMikheilChikvashvili\StockPredictionMikheilChikvashviliML.Model\MLModel.zip";
+            string modelPath = @"MLModel.zip";
             ITransformer mlModel = mlContext.Model.Load(modelPath, out var modelInputSchema);
             var predEngine = mlContext.Model.CreatePredictionEngine<ModelInput, ModelOutput>(mlModel);
 
